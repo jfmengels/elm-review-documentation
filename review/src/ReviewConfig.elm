5f949f92d7c364bfdcbd6102ab553ff5c4dcaeb6
Jeroen Engels <jfm.engels@gmail.com>
2020-04-09 17:07:26 +0200

    Enable a lot of rules :)

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -12,10 +12,31 @@ when inside the directory containing this file.
 -}
 
 import Documentation.ReadmeLinksPointToCurrentVersion
+import NoDebug.Log
+import NoDebug.TodoOrToString
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingTypeAnnotation
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Variables
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
     [ Documentation.ReadmeLinksPointToCurrentVersion.rule
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+        |> Review.Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    , NoMissingTypeAnnotation.rule
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Variables.rule
     ]
