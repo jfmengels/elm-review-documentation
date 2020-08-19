f8cc9aaf0f953fc794dc85e3ece2d01beb9c61a8
Jeroen Engels <jfm.engels@gmail.com>
2020-08-19 20:31:39 +0200

    Refactor how the target is chosen

--- src/Documentation/NoMissing.elm
+++ src/Documentation/NoMissing.elm
@@ -8,6 +8,7 @@ module Documentation.NoMissing exposing (rule)
 
 import Elm.Syntax.Declaration as Declaration exposing (Declaration)
 import Elm.Syntax.Node as Node exposing (Node)
+import Elm.Syntax.Range exposing (Range)
 import Review.Rule as Rule exposing (Error, Rule)
 
 
@@ -62,20 +63,22 @@ declarationVisitor : Node Declaration -> List (Error {})
 declarationVisitor node =
     case Node.value node of
         Declaration.FunctionDeclaration { documentation, declaration } ->
-            checkDocumentation documentation (Node.value declaration).name
+            checkDocumentation
+                documentation
+                (Node.range (Node.value declaration).name)
 
         Declaration.CustomTypeDeclaration { documentation, name } ->
-            checkDocumentation documentation name
+            checkDocumentation documentation (Node.range name)
 
         Declaration.AliasDeclaration { documentation, name } ->
-            checkDocumentation documentation name
+            checkDocumentation documentation (Node.range name)
 
         _ ->
             []
 
 
-checkDocumentation : Maybe (Node String) -> Node a -> List (Error {})
-checkDocumentation documentation name =
+checkDocumentation : Maybe (Node String) -> Range -> List (Error {})
+checkDocumentation documentation range =
     case documentation of
         Just doc ->
             let
@@ -92,7 +95,7 @@ checkDocumentation documentation name =
                     { message = "The documentation is empty"
                     , details = [ "Empty documentation is not useful for the users. Please give explanations or examples." ]
                     }
-                    (Node.range name)
+                    range
                 ]
 
             else
@@ -103,5 +106,5 @@ checkDocumentation documentation name =
                 { message = "Missing documentation"
                 , details = [ "Documentation can help developers use this API." ]
                 }
-                (Node.range name)
+                range
             ]
