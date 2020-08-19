ff37609865d56e77236e2742361f875b28a1974b
Jeroen Engels <jfm.engels@gmail.com>
2020-08-19 20:24:28 +0200

    Refactor Documentation.NoMissing

--- src/Documentation/NoMissing.elm
+++ src/Documentation/NoMissing.elm
@@ -61,98 +61,47 @@ rule =
 declarationVisitor : Node Declaration -> List (Error {})
 declarationVisitor node =
     case Node.value node of
-        Declaration.FunctionDeclaration { declaration, documentation } ->
-            case documentation of
-                Just doc ->
-                    let
-                        trimmedDocumentation : String
-                        trimmedDocumentation =
-                            doc
-                                |> Node.value
-                                |> String.dropLeft 3
-                                |> String.dropRight 2
-                                |> String.trim
-                    in
-                    if trimmedDocumentation == "" then
-                        [ Rule.error
-                            { message = "The documentation is empty"
-                            , details = [ "Empty documentation is not useful for the users. Please give explanations or examples." ]
-                            }
-                            (declaration |> Node.value |> .name |> Node.range)
-                        ]
+        Declaration.FunctionDeclaration { documentation, declaration } ->
+            checkDocumentation documentation (Node.value declaration).name
 
-                    else
-                        []
+        Declaration.CustomTypeDeclaration { documentation, name } ->
+            checkDocumentation documentation name
 
-                Nothing ->
-                    [ Rule.error
-                        { message = "Missing documentation"
-                        , details = [ "Documentation can help developers use this API." ]
-                        }
-                        (declaration |> Node.value |> .name |> Node.range)
-                    ]
-
-        Declaration.CustomTypeDeclaration { name, documentation } ->
-            case documentation of
-                Just doc ->
-                    let
-                        trimmedDocumentation : String
-                        trimmedDocumentation =
-                            doc
-                                |> Node.value
-                                |> String.dropLeft 3
-                                |> String.dropRight 2
-                                |> String.trim
-                    in
-                    if trimmedDocumentation == "" then
-                        [ Rule.error
-                            { message = "The documentation is empty"
-                            , details = [ "Empty documentation is not useful for the users. Please give explanations or examples." ]
-                            }
-                            (Node.range name)
-                        ]
-
-                    else
-                        []
-
-                Nothing ->
-                    [ Rule.error
-                        { message = "Missing documentation"
-                        , details = [ "Documentation can help developers use this API." ]
-                        }
-                        (Node.range name)
-                    ]
-
-        Declaration.AliasDeclaration { name, documentation } ->
-            case documentation of
-                Just doc ->
-                    let
-                        trimmedDocumentation : String
-                        trimmedDocumentation =
-                            doc
-                                |> Node.value
-                                |> String.dropLeft 3
-                                |> String.dropRight 2
-                                |> String.trim
-                    in
-                    if trimmedDocumentation == "" then
-                        [ Rule.error
-                            { message = "The documentation is empty"
-                            , details = [ "Empty documentation is not useful for the users. Please give explanations or examples." ]
-                            }
-                            (Node.range name)
-                        ]
-
-                    else
-                        []
-
-                Nothing ->
-                    [ Rule.error
-                        { message = "Missing documentation"
-                        , details = [ "Documentation can help developers use this API." ]
-                        }
-                        (Node.range name)
-                    ]
+        Declaration.AliasDeclaration { documentation, name } ->
+            checkDocumentation documentation name
 
         _ ->
             []
+
+
+checkDocumentation : Maybe (Node String) -> Node a -> List (Error {})
+checkDocumentation documentation name =
+    case documentation of
+        Just doc ->
+            let
+                trimmedDocumentation : String
+                trimmedDocumentation =
+                    doc
+                        |> Node.value
+                        |> String.dropLeft 3
+                        |> String.dropRight 2
+                        |> String.trim
+            in
+            if trimmedDocumentation == "" then
+                [ Rule.error
+                    { message = "The documentation is empty"
+                    , details = [ "Empty documentation is not useful for the users. Please give explanations or examples." ]
+                    }
+                    (Node.range name)
+                ]
+
+            else
+                []
+
+        Nothing ->
+            [ Rule.error
+                { message = "Missing documentation"
+                , details = [ "Documentation can help developers use this API." ]
+                }
+                (Node.range name)
+            ]
