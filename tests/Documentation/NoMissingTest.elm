81e5c6513f846aa0c875c73da6d3ad43e4ee3310
Jeroen Engels <jfm.engels@gmail.com>
2020-08-19 21:11:09 +0200

    Don't report errors for non-exposed modules

--- tests/Documentation/NoMissingTest.elm
+++ tests/Documentation/NoMissingTest.elm
@@ -1,6 +1,9 @@
 module Documentation.NoMissingTest exposing (all)
 
-import Documentation.NoMissing exposing (everything, rule)
+import Documentation.NoMissing exposing (everything, onlyExposed, rule)
+import Elm.Project
+import Json.Decode as Decode
+import Review.Project as Project exposing (Project)
 import Review.Test
 import Test exposing (Test, describe, test)
 
@@ -179,4 +182,63 @@ import Thing
                             , under = "A"
                             }
                         ]
+        , test "should not report things from non-exposed modules for a package when using onlyExposed" <|
+            \() ->
+                """module NotExposed exposing (..)
+import Thing
+"""
+                    |> Review.Test.runWithProjectData packageProject (rule onlyExposed)
+                    |> Review.Test.expectNoErrors
+        , test "should report things from exposed modules for a package when using onlyExposed" <|
+            \() ->
+                """module Exposed exposing (..)
+import Thing
+"""
+                    |> Review.Test.runWithProjectData packageProject (rule onlyExposed)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = missingMessage
+                            , details = missingDetails
+                            , under = "Exposed"
+                            }
+                        ]
         ]
+
+
+packageProject : Project
+packageProject =
+    Project.new
+        |> Project.addElmJson (createElmJson packageElmJson)
+
+
+packageElmJson : String
+packageElmJson =
+    """
+{
+    "type": "package",
+    "name": "author/package",
+    "summary": "Summary",
+    "license": "BSD-3-Clause",
+    "version": "1.0.0",
+    "exposed-modules": [
+        "Exposed"
+    ],
+    "elm-version": "0.19.0 <= v < 0.20.0",
+    "dependencies": {
+        "elm/core": "1.0.0 <= v < 2.0.0"
+    },
+    "test-dependencies": {}
+}"""
+
+
+createElmJson : String -> { path : String, raw : String, project : Elm.Project.Project }
+createElmJson rawElmJson =
+    case Decode.decodeString Elm.Project.decoder rawElmJson of
+        Ok elmJson ->
+            { path = "elm.json"
+            , raw = rawElmJson
+            , project = elmJson
+            }
+
+        Err _ ->
+            Debug.todo "Invalid elm.json supplied to test"
