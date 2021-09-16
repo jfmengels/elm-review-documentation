b03dc530a4daa6be1316fe68c549a30c6296df8c
Jeroen Engels <jfm.engels@gmail.com>
2021-09-16 21:46:57 +0200

    Add find

--- src/Docs/NoAtDocsMisuse.elm
+++ src/Docs/NoAtDocsMisuse.elm
@@ -71,3 +71,16 @@ commentsVisitor nodes context =
       ]
     , context
     )
+
+find : (a -> Bool) -> List a -> Maybe a
+find predicate list =
+    case list of
+        [] ->
+            Nothing
+
+        first :: rest ->
+            if predicate first then
+                Just first
+
+            else
+                find predicate rest
