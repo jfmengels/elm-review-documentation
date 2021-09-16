7d29c86b1bdf9de00724fa1add8c7d8d698a8fdb
Jeroen Engels <jfm.engels@gmail.com>
2021-09-16 21:41:09 +0200

    Add failing test

--- tests/Docs/NoAtDocsMisuseTest.elm
+++ tests/Docs/NoAtDocsMisuseTest.elm
@@ -8,17 +8,34 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "Docs.NoAtDocsMisuse"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should not report an error when all @docs are correct" <|
             \() ->
-                """module A exposing (..)
+                """module A exposing (D, T, a, b, d)
+
+{-| Bla bla
+
+@docs T, a, b
+@docs d, D
+-}
+a = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should report an error when an element has a @docs reference but element is not exposed" <|
+            \() ->
+                """module A exposing (a, b)
+
+{-| Bla bla
+@docs a, b, unknown
+-}
 a = 1
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "REPLACEME"
+                            { message = "Found @docs reference for non-exposed `unknown`"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "unknown"
                             }
                         ]
         ]
