079902b7a4fb7df90f073d1f20f29792c47fb7ef
Jeroen Engels <jfm.engels@gmail.com>
2021-09-16 21:55:48 +0200

    Add failing test

--- tests/Docs/NoAtDocsMisuseTest.elm
+++ tests/Docs/NoAtDocsMisuseTest.elm
@@ -17,25 +17,50 @@ all =
 @docs T, a, b
 @docs d, D
 -}
+import B
 a = 1
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectNoErrors
-        , test "should report an error when an element has a @docs reference but element is not exposed" <|
+        , test "should report an error when an element has a @docs reference but function is not exposed" <|
             \() ->
                 """module A exposing (a, b)
 
 {-| Bla bla
-@docs a, b, unknown
+@docs a, b, notExposed
 -}
+import B
 a = 1
+b = 2
+notExposed = 3
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "Found @docs reference for non-exposed `unknown`"
+                            { message = "Found @docs reference for non-exposed `notExposed`"
                             , details = [ "REPLACEME" ]
                             , under = "unknown"
                             }
+                        ]
+        , test "should report an error when an element has a @docs reference but type is not exposed" <|
+            \() ->
+                """module A exposing (a, b)
+
+{-| Bla bla
+@docs a, b, NotExposed
+-}
+import B
+a = 1
+b = 2
+type NotExposed = NotExposed
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Found @docs reference for non-exposed `NotExposed`"
+                            , details = [ "REPLACEME" ]
+                            , under = "NotExposed"
+                            }
+                            |> Review.Test.atExactly { start = { row = 4, column = 13 }, end = { row = 4, column = 23 } }
                         ]
         ]
