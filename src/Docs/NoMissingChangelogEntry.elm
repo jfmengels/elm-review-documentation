ffeb8abf6113c9d362821547396682da5b0d6baf
Jeroen Engels <jfm.engels@gmail.com>
2024-06-15 15:13:01 +0200

    Extract function

--- src/Docs/NoMissingChangelogEntry.elm
+++ src/Docs/NoMissingChangelogEntry.elm
@@ -188,45 +188,51 @@ extraFilesVisitor maybeChangelogPath files context =
                         )
 
         Nothing ->
-            case context.elmJsonVersion of
+            ( reportErrorWhenChangelogIsNotFound
+                { changelogPath = maybeChangelogPath
+                , elmJsonVersion = context.elmJsonVersion
+                }
+            , context
+            )
+
+
+reportErrorWhenChangelogIsNotFound : { changelogPath : Maybe String, elmJsonVersion : Maybe String } -> List (Rule.Error scope)
+reportErrorWhenChangelogIsNotFound { changelogPath, elmJsonVersion } =
+    case elmJsonVersion of
+        Nothing ->
+            []
+
+        Just "1.0.0" ->
+            -- TODO Report an error with a changelog skeleton
+            []
+
+        Just _ ->
+            case changelogPath of
                 Nothing ->
-                    ( [], context )
-
-                Just "1.0.0" ->
-                    -- TODO Report an error with a changelog skeleton
-                    ( [], context )
-
-                Just _ ->
-                    case maybeChangelogPath of
-                        Nothing ->
-                            ( [ Rule.globalError
-                                    { message = "Could not find the CHANGELOG.md file"
-                                    , details =
-                                        [ "I was looking for the CHANGELOG.md file next to your project's elm.json file but couldn't find it. Please make sure that the spelling is correct."
-                                        , "If your changelog is named differently or is in a different location, then you can configure this rule to look for it in a different location:"
-                                        , """    config =
+                    [ Rule.globalError
+                        { message = "Could not find the CHANGELOG.md file"
+                        , details =
+                            [ "I was looking for the CHANGELOG.md file next to your project's elm.json file but couldn't find it. Please make sure that the spelling is correct."
+                            , "If your changelog is named differently or is in a different location, then you can configure this rule to look for it in a different location:"
+                            , """    config =
         [ Docs.NoMissingChangelogEntry.defaults
             |> Docs.NoMissingChangelogEntry.withPathToChangelog "path/to/your/changelog.md"
             |> Docs.NoMissingChangelogEntry.rule
         ]"""
-                                        , "Note that the path is relative your project's elm.json file."
-                                        ]
-                                    }
-                              ]
-                            , context
-                            )
+                            , "Note that the path is relative your project's elm.json file."
+                            ]
+                        }
+                    ]
 
-                        Just customPath ->
-                            ( [ Rule.globalError
-                                    { message = "Could not find the " ++ customPath ++ " changelog file"
-                                    , details =
-                                        [ "I was looking for the " ++ customPath ++ " changelog file but couldn't find it. Please make sure that the path you specified through Docs.NoMissingChangelogEntry.withPathToChangelog is correct."
-                                        , "Also note that the path you specify has to be relative to your project's elm.json file."
-                                        ]
-                                    }
-                              ]
-                            , context
-                            )
+                Just customPath ->
+                    [ Rule.globalError
+                        { message = "Could not find the " ++ customPath ++ " changelog file"
+                        , details =
+                            [ "I was looking for the " ++ customPath ++ " changelog file but couldn't find it. Please make sure that the path you specified through Docs.NoMissingChangelogEntry.withPathToChangelog is correct."
+                            , "Also note that the path you specify has to be relative to your project's elm.json file."
+                            ]
+                        }
+                    ]
 
 
 reportError : Rule.ExtraFileKey -> String -> String -> Rule.Error scope
